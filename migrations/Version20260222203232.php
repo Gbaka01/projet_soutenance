bb968ef5921d2672e810dbd914a3af6c1a36ea10
Gbaka01 <gore.goli@gmail.com>
2026-02-22 21:56:50 +0100

    commitgg207

--- migrations/Version20260222203232.php
+++ migrations/Version20260222203232.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20251225132623 extends AbstractMigration
+final class Version20260222203232 extends AbstractMigration
 {
     public function getDescription(): string
     {
